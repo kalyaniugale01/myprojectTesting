dd353f12b0fd36e28501c5d4a70a0d0f83eb3329
kalyani01 <kalyaniugale112gmail.com>
2020-06-30 13:38:12 +0530

    first commit

---
+++ GitDemo/src/com/git/Demo.java
@@ -0,0 +1,12 @@
+package com.git;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello");
+		System.out.println("Git");
+
+	}
+
+}
